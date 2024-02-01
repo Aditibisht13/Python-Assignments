80f1a81e3bffcbfc70aee36bd95d691acd7b0e2e
Aditi <154453925+Aditibisht13@users.noreply.github.com>
2024-02-02 01:52:30 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+
+
+
+# In[ ]:
+
+
+
+
